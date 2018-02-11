8854c048a7f5406ea4555093e9f382c2833d6365
victoriasultanovskaya <victoria.sultanovska@gmail.com>
2018-02-11 01:23:35 +0100

    13.181.Building Real-time Server-less Apps with Firebase: Adding an Object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnDestroy} from '@angular/core';
 import {AngularFireDatabase} from 'angularfire2/database';
 import {Observable} from 'rxjs/Observable';
+import {FirebaseListObservable} from 'angularfire2/database-deprecated';
 
 @Component({
     selector: 'app-root',
@@ -11,6 +12,8 @@ export class AppComponent {
     title = 'app';
 
     coursesObservable: Observable<any[]>;
+    listObservable: FirebaseListObservable<any[]>;
+    lists: FirebaseListObservable<any[]>;
     courseFirst: Observable<any>;
     author$: Observable<any>;
 
@@ -21,5 +24,21 @@ export class AppComponent {
         this.courseFirst = this.db.object('/courses/learn-ionic3-from-scratch').valueChanges();
 
         this.author$ = this.db.object('/authors/1').valueChanges();
+
+        this.listObservable = this.db.list('/list');
+
+        this.lists = this.db.list('/list').valueChanges();
+    }
+
+    add(listItem) {
+        this.listObservable.push({
+            name: listItem.value,
+            sections: {
+                1 : {
+                    name: 'X'
+                }
+            }
+        });
+        listItem.value = '';
     }
 }
