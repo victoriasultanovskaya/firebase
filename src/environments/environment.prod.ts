fabd996262360b7864bd1281b06e2ccfb915c953
Viktoriia Sultanovska <victoria.sultanovska@gmail.com>
2018-03-06 20:23:39 +0100

    Revert "Auto-generated commit"
    This reverts commit 1ad88a5

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyBsCH3pYhjqUF3RVyv8gx7-1c4mN3mJgM8",
+        authDomain: "fir-demo-6aeed.firebaseapp.com",
+        databaseURL: "https://fir-demo-6aeed.firebaseio.com",
+        projectId: "fir-demo-6aeed",
+        storageBucket: "fir-demo-6aeed.appspot.com",
+        messagingSenderId: "742698174067"
+    }
+};
